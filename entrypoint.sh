bef1569693127c0cd71e66a655314c90c542d0c3
CesarSants <cesarsantosplay4@gmail.com>
2025-07-24 15:20:34 -0300

    done 7

--- entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Inicializando Payara Micro com variáveis de ambiente..."
-
-# Executa o Payara Micro usando as variáveis de ambiente já injetadas pelo Railway
-exec java -jar payara-micro-5.2022.5.jar --deploy target/agendamentos-online.war --port $PORT
